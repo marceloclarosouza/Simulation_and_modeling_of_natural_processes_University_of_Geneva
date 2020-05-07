069d835daadb5c0923a2a19aea8b8eccf338b296
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-05-07 12:14:28 -0300

    Create Kinetic_Dynamic_Monte_Carlo_model.py

---
+++ week1/Kinetic_Dynamic_Monte_Carlo_model.py
@@ -0,0 +1,29 @@
+import random
+
+k1 = 0.2
+k2 = 0.2
+deltaT = 2
+numa = 4
+numb = 3
+N = numa + numb
+
+randomNumberList = [0.800, 0.801, 0.752, 0.661, 0.169, 0.956, 0.949, 0.003, 0.201, 0.291, 0.615, 0.131, 0.241, 0.685, 0.116, 0.241, 0.849]
+
+for i in range(len(randomNumberList)):
+    rand = random.choice(randomNumberList)
+    
+    if rand < (numa/N):
+        rand = random.choice(randomNumberList)
+        if rand < (k1*deltaT):
+            numa-=1
+            numb+=1
+        
+        
+    else:
+        rand = random.choice(randomNumberList)
+        if rand < (k2*deltaT):
+            numa+=1
+            numb-=1
+            
+                
+print("num a = {} , num b = {}". format(numa, numb))
