d2758825e342cc1f273b93402e579dd93bb1a983
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-05-07 14:27:26 -0300

    Update Kinetic_Dynamic_Monte_Carlo_model.py

--- week1/Kinetic_Dynamic_Monte_Carlo_model.py
+++ week1/Kinetic_Dynamic_Monte_Carlo_model.py
@@ -9,10 +9,10 @@ Note that in this algorithm, N is the total number of particles.
 It is very important to note that step (4) required N iterations 
 of steps (2) and (3) before incrementing the time by Δt\Delta tΔt. 
 Also, both step (2) and step (3) require one random number. 
-To solve this problem, every time you need a random number pick the
- number in the following list. Start by taking the first number and then
- take the second, etc. Never reuse twice a random number. 
- You should have enough random number to answer the question.
+To solve this problem, every time you need a random number pick 
+the number in the following list. Start by taking the first number 
+and then take the second, etc. Never reuse twice a random number. 
+You should have enough random number to answer the question.
 """
 
 import random
@@ -24,20 +24,22 @@ numa = 4
 numb = 3
 N = numa + numb
 
-randomNumberList = [0.800, 0.801, 0.752, 0.661, 0.169, 0.956, 0.949, 0.003, 0.201, 0.291, 0.615, 0.131, 0.241, 0.685, 0.116, 0.241, 0.849]
+randomNumberList = [0.800, 0.801, 0.752, 0.661, 0.169, 0.956, 0.949, 
+                    0.003, 0.201, 0.291, 0.615, 0.131, 0.241, 0.685, 
+                    0.116, 0.241, 0.849]
 
 for i in range(len(randomNumberList)):
-    rand = float(random.shuffle(randomNumberList))
+    rand = random.choice(randomNumberList)
     
     if rand < (numa/N):
-        rand = float(random.shuffle(randomNumberList))
+        rand = random.choice(randomNumberList)
         if rand < (k1*deltaT):
             numa-=1
             numb+=1
         
         
     else:
-        rand = float(random.shuffle(randomNumberList))
+        rand = random.choice(randomNumberList)
         if rand < (k2*deltaT):
             numa+=1
             numb-=1
